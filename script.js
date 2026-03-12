24db17b2bbc45cf2ecf972b2049530621ebcd91d
Sadik Saifi <sadiksaifi7428@gmail.com>
2026-03-12 13:24:02 +0530

    corect typos

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ let burgerButton = document.getElementById("burger-btn");
 let navMenu = document.getElementById("nav-menu");
 
 burgerButton.addEventlistener("click", function(){
-    navMenu.classList.toggle("open")
+    navMenu.classList.toggle("open");
 });
