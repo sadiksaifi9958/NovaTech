ba8dd560e8119040cdaef9b922f6d16a496f6016
Sadik Saifi <sadiksaifi7428@gmail.com>
2026-03-15 16:45:45 +0530

    add pricing page structure and styling

--- script.js
+++ script.js
@@ -4,3 +4,21 @@ let navMenu = document.getElementById("nav-menu");
 burgerButton.addEventListener("click", function(){
     navMenu.classList.toggle("open");
 });
+
+let cards = document.querySelectorAll(".price-card");
+let cardButton = document.querySelectorAll(".card-button");
+
+cards.forEach(function(card){    
+    card.addEventListener("click", function(){
+        cards.forEach(function(c){
+        c.classList.remove("selected-card");
+    })
+    card.classList.add("selected-card");
+    })
+});
+cardButton.forEach(function(btn){
+    btn.addEventListener("click", function(e){
+        e.stopPropagation();
+    });
+});
+
